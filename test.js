463e39ff6c5316190340b60fb1d1c8720d3b85dc
Yaroslav <eragon2193@gmail.com>
2023-02-05 12:51:12 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function (){ 
+
+document.getElementsByClassName('submit').addEventListener('click', function(){
+  
+  const cpassword = document.getElementById('cpwd')
+  const password = document.getElementById('pwd')
+  if(cpassword.innerText === password.innerText)
+  {
+    alert('log in')
+  }
+    else {
+      alert('make sure password matches')
+    } 
+})})
